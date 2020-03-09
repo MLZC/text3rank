fcec2dd45c88dc7cebdfbac36b0c38878ddb055e
Zhao Chi <dandanv5@hotmail.com>
2020-03-10 00:30:47 +0300

    finished key=Date situation

--- start.py
+++ start.py
@@ -30,44 +30,44 @@ if __name__ == '__main__':
     '''
     test in dictionary
     '''
-    # for k,v in new_dict.items():
-    #     ranker.k=k
-    #     v_list = v[:,1].tolist()
-    #     v_list = pd.Series(v_list).str.replace("http.*", "", regex=True).tolist()
-    #     v_list = pd.Series(v_list).str.replace("pic.twitter.com.*", "", regex=True).tolist()
-    #     np.savetxt("./results/"+k,np.array(v_list),fmt="%s")
-    #     print(k,len(v_list))
-    #     vocabulary,unique_phrases = ranker.build_vocabulary(v_list,txtpath=glove_vec_path)
-    #     print("---"*20+"Ranking"+"---"*20)
-    #     ranker.build_matrix(vocabulary)
-    #     ranker.calc_inout()
-    #     ranker.iteration()
-    #     print("For phrase:\n")
-    #     ranker.ranking(10,1)
-    #     print("For words:\n")
-    #     ranker.ranking(10,0)
-    #     break
+    for k,v in new_dict.items():
+        ranker.k=k
+        v_list = v[:,1].tolist()
+        v_list = pd.Series(v_list).str.replace("http.*", "", regex=True).tolist()
+        v_list = pd.Series(v_list).str.replace("pic.twitter.com.*", "", regex=True).tolist()
+        np.savetxt("./results/"+k,np.array(v_list),fmt="%s")
+        print(k,len(v_list))
+        vocabulary,unique_phrases = ranker.build_vocabulary(v_list,txtpath=glove_vec_path)
+        print("---"*20+"Ranking"+"---"*20)
+        ranker.build_matrix(vocabulary)
+        ranker.calc_inout()
+        ranker.iteration()
+        print("For phrase:\n")
+        ranker.ranking(10,1)
+        print("For words:\n")
+        ranker.ranking(10,0)
+        # break
 
-    '''
-    test for article
-    '''
-    ranker.k="test"
-    Text = "Compatibility of systems of linear constraints over the set of natural numbers. \
-            Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and \
-            nonstrict inequations are considered. \
-            Upper bounds for components of a minimal set of solutions and \
-            algorithms of construction of minimal generating sets of solutions for all \
-            types of systems are given. \
-            These criteria and the corresponding algorithms for constructing \
-            a minimal supporting set of solutions can be used in solving all the \
-            considered types of systems and systems of mixed types."
-    test_str_list = [Text]
-    vocabulary,unique_phrases = ranker.build_vocabulary(test_str_list,txtpath=glove_vec_path)
-    print("---"*20+"Ranking"+"---"*20)
-    ranker.build_matrix(vocabulary)
-    ranker.calc_inout()
-    ranker.iteration()
-    print("For phrase:\n")
-    ranker.ranking(10,1)
-    print("For words:\n")
-    ranker.ranking(10,0)
+#     '''
+#     test for article
+#     '''
+#     ranker.k="test"
+#     Text = "Compatibility of systems of linear constraints over the set of natural numbers. \
+#             Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and \
+#             nonstrict inequations are considered. \
+#             Upper bounds for components of a minimal set of solutions and \
+#             algorithms of construction of minimal generating sets of solutions for all \
+#             types of systems are given. \
+#             These criteria and the corresponding algorithms for constructing \
+#             a minimal supporting set of solutions can be used in solving all the \
+#             considered types of systems and systems of mixed types."
+#     test_str_list = [Text]
+#     vocabulary,unique_phrases = ranker.build_vocabulary(test_str_list,txtpath=glove_vec_path)
+#     print("---"*20+"Ranking"+"---"*20)
+#     ranker.build_matrix(vocabulary)
+#     ranker.calc_inout()
+#     ranker.iteration()
+#     print("For phrase:\n")
+#     ranker.ranking(10,1)
+#     print("For words:\n")
+#     ranker.ranking(10,0)
